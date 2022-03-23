91f21a61d9628a0e39daaba1a2169473d445e4d6
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-03-23 23:01:06 +0530

    serializers done

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -3,7 +3,7 @@ from dataclasses import field
 from importlib.metadata import requires
 from tkinter.ttk import Style
 from turtle import title
-from typing_extensions import Required
+# from typing_extensions import Required
 from urllib import request
 from rest_framework import serializers
 from api_basic.models import Snippet,LANGUAGE_CHOICES,STYLE_CHOICES
