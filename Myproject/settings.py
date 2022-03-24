f2b92cf12a37498ae2037869847b8dcf1a741e30
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-03-24 17:54:25 +0530

    ui updated

--- Myproject/settings.py
+++ Myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_basic'
+    'api_basic',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
